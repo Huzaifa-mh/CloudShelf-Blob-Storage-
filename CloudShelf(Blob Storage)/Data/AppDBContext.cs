eaeb69ec4f47929d53c5622b3319dd86b260724f
Muhammad Huzaifa <muhammadhuzaifa_mh@yahoo.com>
2026-04-24 19:52:05 +0500

    Setting up the connection string of the azure blob storage and ssms , working with models and the Db Context for the code first migration approach

---
+++ CloudShelf(Blob Storage)/Data/AppDBContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CloudShelf_Blob_Storage_.Models;
+
+namespace CloudShelf_Blob_Storage_.Data
+{
+    public class AppDBContext(DbContextOptions<AppDBContext> options ) : Microsoft.EntityFrameworkCore.DbContext (options)
+    {
+
+        public DbSet<Models.image> Images => Set<image>();
+    }
+}
---
+++ CloudShelf(Blob Storage)/Models/image.cs
@@ -0,0 +1,12 @@
+﻿namespace CloudShelf_Blob_Storage_.Models
+{
+    public class image
+    {
+        public int Id { get; set; }
+        public string FileName { get; set; }
+
+        public string OriginalUrl { get; set; }
+        public string? ContentType {get; set;}
+        public DateTime UploadedAt { get; set; } = DateTime.UtcNow;
+    }
+}
