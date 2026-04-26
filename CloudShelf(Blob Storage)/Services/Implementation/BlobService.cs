e1c989f2f176a6d09e59d9ee342c74e760814788
Muhammad Huzaifa <muhammadhuzaifa_mh@yahoo.com>
2026-04-26 20:27:55 +0500

    setting up for the connection of azure blob storage

---
+++ CloudShelf(Blob Storage)/Services/Implementation/BlobService.cs
@@ -0,0 +1,31 @@
+﻿using Azure.Storage.Blobs;
+using CloudShelf_Blob_Storage_.Services.Interface;
+
+namespace CloudShelf_Blob_Storage_.Services.Implementation
+{
+    public class BlobService : IBlobService
+    {
+        private readonly string _connectionString;
+        private readonly string _containerName;
+        private readonly string _accountName;
+        private readonly string _accountKey;
+
+        public  BlobService(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("AzureStorage:ConnectionString");
+            _containerName = configuration["AzureStorage:ContainerName"];
+            _accountName = configuration["AzureStorage:AccountName"];
+            _accountKey = configuration["AzureStorage:AccountKey"];
+        }
+
+        public async Task<string> UploadBlobAsync(Stream imageStream, string fileName, string contentType)
+        {
+            //connect to azure
+            var blobServiceClient = new BlobServiceClient(_connectionString);
+
+            //get your container
+            var containerClient = blobServiceClient.GetBlobContainerClient(_containerName);
+
+        }
+    }
+}
---
+++ CloudShelf(Blob Storage)/Services/Interface/IBlobService.cs
@@ -0,0 +1,8 @@
+﻿namespace CloudShelf_Blob_Storage_.Services.Interface
+{
+    public interface IBlobService
+    {
+        Task<string> UploadBlobAsync(Stream imageStream, string fileName, string contentType);
+        string GenerateSasUrl(string fileName);
+    }
+}
